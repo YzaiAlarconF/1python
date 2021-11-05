40e4f3878c559c2573fcef29d9bd5c00c3a0dd8b
Yzai Alarcon Franca <alarconfrancay@gmail.com>
2021-11-05 00:00:52 -0600

    Primer programa en python

---
+++ PythonApplication1.py
@@ -0,0 +1,16 @@
+INICIO = 0
+FIN = -1
+INDICE = 2
+SALTO = 1
+
+LIST_LISTA =  ("HOLA",'SOY','ROKUX',[1,2],3)
+print("\n***************Elemento de la lista*******************************\n")
+
+print(LIST_LISTA[0])
+print(LIST_LISTA[1])
+print(LIST_LISTA[2])
+print(LIST_LISTA[3])
+print(LIST_LISTA[3][0])
+print(LIST_LISTA[3][1])
+print(LIST_LISTA[-1])
+print(LIST_LISTA[-2])
